2ffd65f42bee551649c49ac9dcc36c4eb619a7d0
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2013-03-12 16:17:09 -0300

    201303121617-LTI-PROJETO-TM

--- keepaway.sh
+++ keepaway.sh
@@ -23,14 +23,14 @@ keeper_load_dir=                 # sub-directory of weight_dir where weights are
 
 keeper_load_PRQL=1               # should I load previously learned weights to use in PRQL?
 
-num_keepers_reuse=3              # numbers of players that will reuse policies
+num_keepers_reuse=4              # numbers of players that will reuse policies
 
-keeper_load_PRQL_3v2=1           # should I load previously learned weights to use in PRQL?
-keeper_load_PRQL_4v3=0           # should I load previously learned weights to use in PRQL?
+keeper_load_PRQL_3v2=0           # should I load previously learned weights to use in PRQL?
+keeper_load_PRQL_4v3=1           # should I load previously learned weights to use in PRQL?
 keeper_load_PRQL_Abs=0           # should I load previously learned weights to use in PRQL?
 
-keeper_load_PRQL_3v2_dir=201302131649-LTI-PROJETO-TM                 # sub-directory of weight_dir where weights are stored
-keeper_load_PRQL_4v3_dir=                                            # sub-directory of weight_dir where weights are stored
+keeper_load_PRQL_3v2_dir=                                            # sub-directory of weight_dir where weights are stored
+keeper_load_PRQL_4v3_dir=201303121554-LTI-PROJETO-TM                 # sub-directory of weight_dir where weights are stored
 keeper_load_PRQL_Abs_dir=                                            # sub-directory of weight_dir where weights are stored
 
 keeper_learn=1                   # should learning be turned on for keepers?
